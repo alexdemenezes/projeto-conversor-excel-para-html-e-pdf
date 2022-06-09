7e4a9f7c624a95953a84d9cbcb98ae95a3b03e7b
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-09 17:33:17 -0300

    criacao da classe Table

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const Reader = require('./Reader');
 const Processor = require('./Processor');
-
+const Table = require('./Table');
 const leitor = new Reader();
 
-const data = leitor.Read('./users.csv');
+function main() {
+  const data = leitor.Read('./users.csv');
+  const processedData = Processor.Process(data);
 
-Processor.Process(data);
+  const users = new Table(processedData);
+
+  console.log(users.RowCount)
+  console.log(users.ColumnCount);
+}
+
+main();
